1508f40cd07796c7522b3d4e8de19fd934fe7d6c
yangkun <yangkun@icourt.cc>
2024-05-27 18:15:57 +0800

    feat: 新增全局clipboard hooks

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script lang="tsx" setup>
 import VerifyWaterMark from './_test_/VerifyWaterMark.vue';
+import VerifyClipBoard from './_test_/VerifyClipBoard.vue';
 </script>
 <template>
-<div>
-  <VerifyWaterMark></VerifyWaterMark>
-</div>
+  <div>
+    <VerifyWaterMark></VerifyWaterMark>
+    <VerifyClipBoard></VerifyClipBoard>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
---
+++ src/_test_/VerifyClipBoard.vue
@@ -0,0 +1,21 @@
+<script lang="tsx" setup>
+// import { useWatermark } from '@/lib/useWatermark/index';
+import { onMounted, ref } from 'vue';
+import { useClipboard } from '../lib/useClipBoard/index';
+
+const inputText = ref('');
+
+const { text, isSupported, copy } = useClipboard();
+
+onMounted(() => {
+  console.log('mounted');
+  // const { text, isSupported, copy } = useClipboard({ read: true });
+});
+</script>
+<template>
+  <p>
+    Current copied: <code>{{ text || 'none' }}</code>
+  </p>
+  <input v-model="inputText" type="text" />
+  <button @click="copy(inputText)">Copy</button>
+</template>
